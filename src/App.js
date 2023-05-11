ba08e26a54f7eb9e91a38bb2fb598e4b71fc0e6f
Florent Mamprin <florentmamprin@gmail.com>
2023-05-11 22:27:11 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" width={200} />
-        <img src={damiens} alt="logo" width={2500}/>
+        <img src={damiens} alt="logo" width={200}/>
       </header>
     </div>
   );
